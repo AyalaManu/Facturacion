37305239b32b9cca0e291aea6ee28fe4cf629415
AyalaManu <114090237+AyalaManu@users.noreply.github.com>
2022-10-06 21:10:14 -0500

    Facturacion(3ro)
    Tercer commit

--- facturacion/src/com/tuempresa/facturacion/modelo/cliente.java
+++ facturacion/src/com/tuempresa/facturacion/modelo/cliente.java
@@ -1,24 +1,23 @@
 package com.tuempresa.facturacion.modelo;
+
 import javax.persistence.*;
 
 import org.openxava.annotations.*;
 
 import lombok.*;
 
-@Entity
-@Getter @Setter
-public class cliente {
-	@Id 
+@Entity @Getter @Setter
+@View(name="Simple",members = "numero, nombre")
+public class Cliente {
 
-	@Column(length=6)
+	@Id
+	@Column(length = 6)
 	int numero;
-
+	
 	@Column(length = 50)
 	@Required
 	String nombre;
 	
-	@Embedded  
+	@Embedded
 	Direccion direccion;
-	
-	
 }
